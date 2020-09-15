0b953c1bb5aae1164ae05aa1ec5677788b58d80d
Suerta <suerta__@hotmail.com>
2020-09-15 17:59:16 +0800

    [Zhang Jindong] feat: delete useless import

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,6 +1,6 @@
 package com.thoughtworks.rslist;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.domain.RsEvent;
 import org.junit.jupiter.api.Test;
