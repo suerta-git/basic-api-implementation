cdf43ffad358810156c927803229e17c42663548
Suerta <suerta__@hotmail.com>
2020-09-15 17:38:34 +0800

    [Zhang Jindong] feat: exercise on class

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,34 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private final List<RsEvent> rsList = new ArrayList<>(Arrays.asList(
+          new RsEvent("第一条事件", "无标签"),
+          new RsEvent("第二条事件", "无标签"),
+          new RsEvent("第三条事件", "无标签")));
+
+  @GetMapping("/rs/list")
+  public List<RsEvent> getRsList(@RequestParam (required = false) Integer start, @RequestParam (required = false) Integer end) {
+    if (start == null || end == null) {
+      return rsList;
+    }
+    return rsList.subList(start - 1, end);
+  }
+
+  @PostMapping("/rs/event")
+  public void addRsEvent(@RequestBody RsEvent rsEvent){
+    rsList.add(rsEvent);
+  }
+
+  @GetMapping("/rs/{index}")
+  public RsEvent getOneRsEvent(@PathVariable int index) {
+    return rsList.get(index - 1);
+  }
 }
