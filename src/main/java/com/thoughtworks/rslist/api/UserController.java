bf5cf1065d9794858dbd52cf610584c239ccac22
Suerta <suerta__@hotmail.com>
2020-09-17 00:35:35 +0800

    [Zhang Jindong] feat: should print log

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -3,6 +3,8 @@ package com.thoughtworks.rslist.api;
 import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.exception.Error;
 import com.thoughtworks.rslist.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,6 +18,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/user")
     public List<User> getUserList() {
         return userService.getUserList();
@@ -28,6 +32,7 @@ public class UserController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     private ResponseEntity<Error> exceptionHandler(MethodArgumentNotValidException e) {
+        logger.error("Here is a invalid user");
         return ResponseEntity.badRequest().body(new Error("invalid user"));
     }
 }
--- src/main/java/com/thoughtworks/rslist/component/RsEventExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/component/RsEventExceptionHandler.java
@@ -3,6 +3,8 @@ package com.thoughtworks.rslist.component;
 import com.thoughtworks.rslist.api.RsController;
 import com.thoughtworks.rslist.exception.Error;
 import com.thoughtworks.rslist.exception.RsEventNotValidException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice(assignableTypes = {RsController.class})
 public class RsEventExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(RsEventExceptionHandler.class);
+
     @ExceptionHandler(RsEventNotValidException.class)
     public ResponseEntity<Error> exceptionHandler(RsEventNotValidException e) {
+        logger.error("Here is a " + e.getMessage());
         return ResponseEntity.badRequest().body(new Error(e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     private ResponseEntity<Error> exceptionHandler(MethodArgumentNotValidException e) {
+        logger.error("Here is a invalid param");
         return ResponseEntity.badRequest().body(new Error("invalid param"));
     }
 }
