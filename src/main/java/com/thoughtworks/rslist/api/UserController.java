5683b3bac89065b3c0d10a754ad3c7c6cd7a49a7
Suerta <suerta__@hotmail.com>
2020-09-16 15:21:50 +0800

    [Zhang Jindong] feat: should add user without validation

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -2,6 +2,8 @@ package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,5 +21,10 @@ public class UserController {
     @GetMapping("/user")
     public List<User> getUserList() {
         return userList;
+    }
+
+    @PostMapping("/user")
+    public void addUser(@RequestBody User user){
+        userList.add(user);
     }
 }
--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -2,10 +2,14 @@ package com.thoughtworks.rslist.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,12 +25,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @Order(2)
     void should_get_user_list() throws Exception {
         List<User> userList = new ArrayList<>(Arrays.asList(
                 new User("user1", 20, "male", "user1@test.com", "18888888888"),
@@ -35,6 +41,29 @@ class UserControllerTest {
         ));
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(userList);
+
+        mockMvc.perform(get("/user"))
+                .andExpect(content().json(jsonString))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(1)
+    void should_add_user_without_validation() throws Exception {
+        User newUser = new User("newUser", 20, "male", "new@test.com", "18888888888");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(newUser);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        List<User> userList = new ArrayList<>(Arrays.asList(
+                new User("user1", 20, "male", "user1@test.com", "18888888888"),
+                new User("user2", 20, "female", "user2@test.com", "18888888888"),
+                new User("user3", 20, "female", "user3@test.com", "18888888888"),
+                newUser
+        ));
+        jsonString = objectMapper.writeValueAsString(userList);
 
         mockMvc.perform(get("/user"))
                 .andExpect(content().json(jsonString))
