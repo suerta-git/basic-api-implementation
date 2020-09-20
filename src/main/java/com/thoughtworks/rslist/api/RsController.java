cd65d3e481d34d7c407132d17e33f83d64e47aae
Suerta <suerta__@hotmail.com>
2020-09-20 18:57:31 +0800

    [Zhang Jindong] feat: use constructor to inject dependencies

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,17 +1,18 @@
 package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.service.RsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  @Autowired
-  RsService rsService;
+  private final RsService rsService;
+
+  public RsController(RsService rsService) {
+    this.rsService = rsService;
+  }
 
   @GetMapping("/list")
   public List<String> getList() {
